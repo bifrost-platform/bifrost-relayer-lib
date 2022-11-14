eb0a138fb247ce0022a7b99b452be358623b61d8
dongchangYoo <yoodc8167@gmail.com>
2022-11-14 15:41:09 +0900

    test setup.py5

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     name="bifrost-relayer-lib",
     version="v0.2.0",
     package=find_packages,
-    install_requires=[
-        requirements
-    ]
+    install_requires=[],
+    dependency_links=["git+https://github.com/bifrost-platform/bifrost-python-lib.git"]
 )

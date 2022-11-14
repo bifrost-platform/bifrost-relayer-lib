ce62bb9291d4be2ca616fe4307b8d0d1dc07f8dd
dongchangYoo <yoodc8167@gmail.com>
2022-11-14 14:30:43 +0900

    fix typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bifrost-python-lib",
+    name="bifrost-relayer-lib",
     version="v0.2.0",
     package=find_packages,
     install_requires=[

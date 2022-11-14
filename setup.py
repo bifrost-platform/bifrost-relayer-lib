619ffb974aa896d227eac2f7e5434628bde2b80e
dongchangYoo <yoodc8167@gmail.com>
2022-11-14 15:36:50 +0900

    test setup.py4

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="bifrost-relayer-lib",
     version="v0.2.0",
     package=find_packages,
     install_requires=[
-        "git@github.com:bifrost-platform/bifrost-python-lib.git"
+        requirements
     ]
 )
